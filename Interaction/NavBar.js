2beb38c39c276187b1dc9429e7771bce7b4f732d
Shreyas Jayaprakash <126165002+shreyasjp@users.noreply.github.com>
2024-01-17 06:45:18 +0530

    Full

--- Interaction/NavBar.js
+++ Interaction/NavBar.js
@@ -4,3 +4,10 @@ var $nav = $("nav");
 $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
 });
+
+function downloadPdf(pdfPath, pdfName) {
+    var link = document.createElement('a');
+    link.href = pdfPath;
+    link.download = pdfName;
+    link.click();
+  }   
