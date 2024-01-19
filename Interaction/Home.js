05678a5e8295684b9b3b783d1fcfca7476936a8e
Shreyas Jayaprakash <126165002+shreyasjp@users.noreply.github.com>
2024-01-20 02:09:34 +0530

    Alpha Plus

--- Interaction/Home.js
+++ Interaction/Home.js
@@ -36,3 +36,48 @@ function filterEvents(selectedFilters) {
     tile.classList.toggle('hide', !isVisible);
   });
 }
+
+const modal = document.getElementById('doc-modal');
+    const posterDisplay = document.getElementById('poster-display');
+
+    // Get references to event posters
+    const eventPosters = document.querySelectorAll('.event-poster');
+
+    // Add click event listener to each event poster
+    eventPosters.forEach((poster) => {
+        poster.addEventListener('click', () => {
+            // Get the source of the clicked poster
+            const posterSource = poster.getAttribute('src');
+
+            // Set the source of the modal's poster display
+            posterDisplay.setAttribute('src', posterSource);
+
+            // Display the modal and overlay
+            modal.classList.remove('hide');
+        });
+    });
+
+    // Close modal and overlay when clicking outside the modal
+    modal.addEventListener('click', () => {
+        modal.classList.add('hide');
+    });
+
+    function handleLoad() {
+      const loader = document.getElementById("page-loader");
+      const content = document.getElementById("container");
+      const navBar = document.querySelector("nav");
+      const footer = document.querySelector(".footer");
+  
+      if (loader && content) {
+      loader.remove();
+      content.classList.remove("hide");
+      if (navBar){
+          navBar.classList.remove("hide");
+      }
+      if (footer){
+          footer.classList.remove("hide");
+      }
+      }
+  }
+  
+  window.addEventListener("load", handleLoad);
