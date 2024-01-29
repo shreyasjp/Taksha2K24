f4d57f14f4181223effa36e57e9f7c8ed17dd7e9
Shreyas Jayaprakash <126165002+shreyasjp@users.noreply.github.com>
2024-01-29 21:58:40 +0530

    Beta Beta

---
+++ Interaction/Carousel.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector(".carousel");
+  const carouselWrapper = document.querySelector(".carousel-wrapper");
+
+  let currentIndex = 0;
+
+  function updateCarousel() {
+    if (currentIndex === 0) {
+      document.getElementById("elites").classList.add("smallCarousel");
+      document.getElementById("farmfed").classList.add("largeCarousel");
+    } else {
+      document.getElementById("farmfed").classList.remove("largeCarousel");
+      document.getElementById("elites").classList.remove("smallCarousel");
+    }
+    const translateValue = -currentIndex * 50 + "%";
+    carouselWrapper.style.transform = "translateX(" + translateValue + ")";
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % 3; // 6 is the total number of images
+    updateCarousel();
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + 6) % 6;
+    updateCarousel();
+  }
+
+  setInterval(function () {
+    nextSlide();
+  }, 3000); // Adjust the interval for automatic scrolling
+
+  // Optional: Uncomment the lines below if you want to enable manual navigation using buttons
+  // const nextButton = document.getElementById("nextButton");
+  // const prevButton = document.getElementById("prevButton");
+
+  // nextButton.addEventListener("click", nextSlide);
+  // prevButton.addEventListener("click", prevSlide);
+});
