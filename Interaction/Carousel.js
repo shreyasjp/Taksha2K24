4d04c8df6634c880a0fde51a7e6bba59dcdabe2e
Shreyas Jayaprakash <126165002+shreyasjp@users.noreply.github.com>
2024-01-30 18:52:09 +0530

    Final Commit

--- Interaction/Carousel.js
+++ Interaction/Carousel.js
@@ -5,19 +5,24 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentIndex = 0;
 
   function updateCarousel() {
-    if (currentIndex === 0) {
+    /* if (currentIndex === 0) {
       document.getElementById("elites").classList.add("smallCarousel");
       document.getElementById("farmfed").classList.add("largeCarousel");
     } else {
       document.getElementById("farmfed").classList.remove("largeCarousel");
       document.getElementById("elites").classList.remove("smallCarousel");
-    }
+    } */
     const translateValue = -currentIndex * 50 + "%";
     carouselWrapper.style.transform = "translateX(" + translateValue + ")";
   }
 
   function nextSlide() {
-    currentIndex = (currentIndex + 1) % 3; // 6 is the total number of images
+    if (currentIndex === 3) {
+      currentIndex = (currentIndex + 2) % 6; // 6 is the total number of images
+      updateCarousel();
+      return true
+    }
+    currentIndex = (currentIndex + 1) % 6; // 6 is the total number of images
     updateCarousel();
   }
 
